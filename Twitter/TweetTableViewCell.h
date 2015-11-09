30e9bc2f6f8ebe0c3f891123ce512306020862db
Alex Lester <alester@groupon.com>
2015-11-10 06:48:30 +1100

    Got rows showing

---
+++ Twitter/TweetTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  TweetTableViewCell.h
+//  Twitter
+//
+//  Created by Alex Lester on 11/10/15.
+//  Copyright © 2015 Alex Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *TweetTextLabel;
+
+@end
--- Twitter/TweetsViewController.m
+++ Twitter/TweetsViewController.m
@@ -10,13 +10,16 @@
 #import "User.h"
 #import "Tweet.h"
 #import "TwitterClient.h"
+#import "TweetTableViewCell.h"
 
 
-@interface TweetsViewController ()
+@interface TweetsViewController ()<UITableViewDataSource, UITableViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UITableView *tweetTableVIew;
 @end
 
 @implementation TweetsViewController
+
 - (IBAction)onLogout:(id)sender {
 	[User logout];
 	
@@ -30,11 +33,33 @@
 			NSLog(@"text: %@", tweet.text);
 		}
 	}];
+	
+	self.tableView.delegate = self;
+	self.tableView.dataSource = self;
+	
+		[self.tableView registerNib:[UINib nibWithNibName:@"TweetTableViewCell" bundle:nil] forCellReuseIdentifier:@"TweetCell"];
+	
+	[self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+	static NSString *CellIdentifier = @"TweetCell";
+		TweetTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TweetCell"];
+	if (!cell) {
+		cell = [[TweetTableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"TweetCell"];
+	}
+		cell.TweetTextLabel.text = @"Hi I'm a tweet";
+		//cell.delegate = self;
+		return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return 100;
 }
 
 /*
